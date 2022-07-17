4335cc8ecacb699c6eacc4c84aae21b32ff874b1
marco12-star <99267663+Anvar2742@users.noreply.github.com>
2022-07-17 12:35:37 +0200

    Hero section animation is done.

--- js/main.js
+++ js/main.js
@@ -4,26 +4,51 @@ var startWidth = startCircle.offsetWidth
 var size = 150,
     increasingSize = size
 var searchBtn = document.getElementById('searchBtn')
-// var background = '#fff';
+var background = '#fff';
 var border = '3px solid rgb(223 223 223)';
+var heroNfts = document.getElementById('heroNfts')
 
+if (window.innerWidth <= 576) {
+    size = 100
+}
 window.addEventListener("load", function () {
     setTimeout(() => {
-        // startCircle.style.background = background
-        startCircle.style.background = border
+        startCircle.style.background = background
+        startCircle.style.border = border
         for (let i = 1; i < circles.children.length; i++) {
-            const el = circles.children[i]
-            var elLast = circles.children[circles.children.length - i]
+            var elLast = circles.children[circles.children.length - i - 1]
+
+            // Reduce transition for each element
+            elLast.style.transition = 1.3 - i / 10 + `s linear`
+            // elLast.style.transition = i / 10 + .6 + `s cubic-bezier(${1 - i / 10}, 0.82, 0.68, 0.19) 0s`
 
             // Increase size of circle
             increasingSize += size
             elLast.style.width = increasingSize + 'px'
             elLast.style.height = increasingSize + 'px'
 
-            // Decrease opacity of circle
+            // Make style adjustments
             elLast.style.opacity = 100 - (i * 10) + '%'
-            // elLast.style.background = background
+            elLast.style.background = background
             elLast.style.border = border
         }
     }, 500);
+    setTimeout(() => {
+        for (let i = 0; i < circles.children.length; i++) {
+            var el = circles.children[i]
+
+            el.style.transition = .15 + i / 10 + `s linear`
+            el.classList.add('circle_shadow')
+        }
+    }, 1200);
+    setTimeout(() => {
+        heroNfts.classList.add('floating__nfts')
+    }, 1500);
+
+    // for (let i = 0; i < heroNfts.children.length; i++) {
+    //     const el = heroNfts.children[i];
+
+    //     // el.style.top = Math.round(Math.random() * 100) + "%"
+    //     // el.style.left = Math.round(Math.random() * 100) + "%"
+    // }
 })
